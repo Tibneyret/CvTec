1411f78833d5fe01e90c5c6c475bef45300b9ac8
Tibneyret <thibaud.neyret@gmail.com>
2018-10-29 14:13:00 -0400

    implementReact :construction: linkedin oauth work with react component

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,62 @@
 
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { Dashboard } from './components/Dashboard/Dashboard.js';
-import { Login } from './components/Login/Login.js';
-import { PrivateRoute } from './components/PrivateRoute.js';
+
+import SocialButton from './components/SocialButton/SocialButton'
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { isAuthenticated: false, user: null, token: '' }
+    // this.onSuccess = this.onSuccess.bind(this);
+    // this.onFailed = this.onFailed.bind(this);
+  }
+  onSuccess = (response) => {
+    const token = response.token.accessToken;
+    if (token) {
+      this.setState({ isAuthenticated: true, user: response.profile, token: token });
+    }
+  };
+
+  logout = () => {
+    this.setState({ isAuthenticated: false, token: '', user: null })
+  };
+
+
+  onFailed = (error) => {
+    alert(error);
+  };
+
   render() {
+    let content = !!this.state.isAuthenticated ?
+      (
+        <div>
+          <p>Authenticated</p>
+          <div>
+            {this.state.user.email}
+          </div>
+          <div>
+            <button onClick={this.logout} className="button" >
+              Log out
+            </button>
+          </div>
+        </div>
+      ) :
+      (
+        <SocialButton
+          provider='linkedin'
+          appId='7773ywmrf2wxgx'
+          onLoginSuccess={this.onSuccess}
+          onLoginFailure={this.onFailed}
+        >
+          Login with linkedin
+    </SocialButton>
+      );
+
     return (
       <div className="App">
-        <div className="App-content">
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <PrivateRoute path='/dashboard' component={Dashboard} />
-          </Switch>
-        </div>
+        {content}
       </div>
     );
   }
