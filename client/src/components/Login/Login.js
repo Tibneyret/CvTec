7519286ebb5d6abd1e038d7a196f1e07b6fe45cf
Tibneyret <thibaud.neyret@gmail.com>
2018-11-16 14:53:45 -0500

    :constructor: :hammer: change linkedinLibray , Use the official Linkedinn javascript SDK now

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import SocialButton from '../SocialButton/SocialButton';
-import linkedinConfig from '../../resources/_linkedinConfig';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -20,53 +18,10 @@ const styles = {
 class Login extends React.Component {
   constructor(props) {
     super();
-    this.state = { isAuthenticated: false, user: null, token: '' };
-    this.onSuccess = this.onSuccess.bind(this);
-    this.onFailed = this.onFailed.bind(this);
   }
-
-  onSuccess = (response) => {
-    const token = response.token.accessToken;
-    if (token) {
-      this.setState({ isAuthenticated: true, user: response.profile, token: token });
-    }
-  };
-
-  logout = () => {
-    this.setState({ isAuthenticated: false, token: '', user: null });
-  };
-
-
-  onFailed = (error) => {
-    alert(error);
-  };
 
   render() {
     const { classes } = this.props;
-
-    let content = this.state.isAuthenticated ?
-      (
-        <div>
-          <div>
-            {this.state.user.email}
-          </div>
-          <div>
-            <Button onClick={this.logout}>
-              Log out
-            </Button>
-          </div>
-        </div>
-      ) :
-      (
-        <SocialButton
-          provider="linkedin"
-          appId={linkedinConfig.clientID}
-          onLoginSuccess={this.onSuccess}
-          onLoginFailure={this.onFailed}
-        >
-          Login with linkedin
-        </SocialButton>
-      );
 
     return (
       <div className={classes.root} >
@@ -76,7 +31,10 @@ class Login extends React.Component {
               Make Your Resume
             </Typography>
             <div className="Login">
-              {content}
+              <SocialButton
+              >
+                Login with linkedin
+              </SocialButton>
             </div>
           </Toolbar>
         </AppBar >
--- client/src/components/SocialButton/SocialButton.js
+++ client/src/components/SocialButton/SocialButton.js
@@ -1,11 +1,76 @@
 import React from 'react';
-import SocialLogin from 'react-social-login';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const loginButton = ({ children, triggerLogin, ...props }) => (
-  <Button onClick={triggerLogin} {...props}>
-    {children}
-  </Button>
-);
+// load LINKEDIN Library
+const IN = window.IN;
 
-export default SocialLogin(loginButton);
+const styles = {
+  logoutButton: {
+    color: '#FFF',
+  },
+
+};
+
+class loginButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onLoadLogIn = this.onLoadLogIn.bind(this);
+
+    this.state = {
+      isAuthenticated: false,
+      linkedinUser: null,
+    };
+  }
+
+  // Handle the successful return from the API call
+  onSuccess = (data) => {
+    console.log(data);
+    this.setState({
+      isAuthenticated: true,
+      linkedinUser: data
+    });
+  }
+
+  // Handle an error response from the API call
+  onError = (error) => {
+    console.log(error);
+  }
+
+  // Trigger when click on linkedinButton
+  onLoadLogIn = () => {
+    if (!this.state.isAuthenticated) {
+      IN.Event.on(IN, "auth", this.getProfileData);
+    } else {
+      IN.User.logout();
+      this.setState({
+        isAuthenticated: false,
+        linkedinUser: null
+      });
+    }
+  }
+
+  // Request linkedin profile
+  getProfileData = () => {
+    IN.API.Raw("/people/~").result(this.onSuccess).error(this.onError);
+  }
+
+  render() {
+
+    const { classes } = this.props;
+    return (
+      <Button onClick={this.onLoadLogIn} className={classes.logoutButton}>
+        <script type="in/Login">Log Out</script>
+      </Button >
+    );
+  }
+}
+
+loginButton.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(loginButton);
