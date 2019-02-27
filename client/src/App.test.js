bc16d74e286bc066736d5b027d0ac6804e01e123
Tibneyret <thibaud.neyret@gmail.com>
2019-02-27 16:12:06 -0500

    :rotating_light: -front- test app

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+
+it('renders App without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
